c5e3e22d114a3a13df58ccd369a636cb38104625
wahyuwww <wahyu.aan2508@gmail.com>
2022-06-15 17:28:19 +0700

    respone

--- src/controller/authEmployee.js
+++ src/controller/authEmployee.js
@@ -96,8 +96,9 @@ const refreshToken = (req, res, next) => {
 const updateProfileEmployee = async (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    const decoded = jwt.verify(token, process.env.SECRET_KEY_JWT2);
     const idemployee = decoded.id;
+    console.log(idemployee)
     const gambars = req.file.path
     // console.log(req.file)
     const ress = await cloudinary.uploader.upload(gambars)
