c054477c6e236861da25b242cbcb7ed9a8580eb3
wahyuwww <wahyu.aan2508@gmail.com>
2022-06-16 07:41:33 +0700

    update

--- src/routes/company.js
+++ src/routes/company.js
@@ -5,5 +5,6 @@ const Router = express.Router()
 Router.get('/', companyController.getCompany)
   .get('/employee/:id', companyController.getCompanyByEmployee)
   .get('/filter', companyController.getCompanyByFilter)
+  .get('/idcompany', companyController.getDetailCompany)
 
 module.exports = Router
